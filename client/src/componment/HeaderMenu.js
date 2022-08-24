2aeb0f46773a4a2fd25db079edde0cc4f40a29d8
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2022-08-25 01:44:25 +0700

    Update HeaderMn Css

--- client/src/componment/HeaderMenu.js
+++ client/src/componment/HeaderMenu.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import logo from '../Assets/logo/logo.png'
 
 function HeaderMenu() {
   return (
-    <div>HeaderMenu</div>
+    <div className='header-mn-container'>
+        <div className='header-mn-left'>
+            <a href="/">
+                <img src={logo} alt="Shop-Logo" width="120px"/>
+            </a>
+        </div>
+        <div className='header-mn-mid'>
+            <ul>
+                <li>Sản Phẩm</li>
+                <li>Sale</li>
+                
+            </ul>
+        </div>
+        <div className='header-mn-right'>
+            <ul>
+                <li>Giỏ Hàng</li>
+                <li>Đăng nhập</li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
--- client/src/componment/MainBanner.js
+++ client/src/componment/MainBanner.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import banner from '../Assets/img/Banner2.jpg'
 
 function MainBanner() {
   return (
     <div>
-
+        <img src={banner} alt="Banner" width="100%" />
     </div>
   )
 }
