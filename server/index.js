548c37692b45c8c659592bec64938bc5c45b548b
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2022-09-11 17:46:23 +0700

    Created db connection and get product res

--- server/index.js
+++ server/index.js
@@ -5,3 +5,22 @@ const app = express();
 app.listen('3001',()=>{
     console.log("server is running on port 3001");
 })
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: 'db_daisy_boutique'
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log(" Database Connected!");
+  });
+
+  app.get("/getpr",(req,res)=>{
+    con.query("SELECT * FROM `tb_product`",function(err,result,fields){
+        if(err) throw err; 
+        res.send(result);
+    })
+  })
