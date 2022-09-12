548ba5c68ff8fce11a867f346e9cb4f7d5938dcf
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2022-09-12 14:16:26 +0700

    add logo and stylling to footer

--- client/src/componment/FooterSection.js
+++ client/src/componment/FooterSection.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import logo from "../Assets/logo/logo.png";
 
 function FooterSection() {
   return (
-    <div className='footer-mn'>FooterSection</div>
+    <div className='footer-mn'>
+      <div className='footer-container'>
+        <div className='footer-left'>
+          <img src={logo} alt="logo" width="120px" />
+        </div>
+        <div className='footer-mid'>mid</div>
+        <div className='footer-right'>right</div>
+      </div>
+      <h5>@Copy-righted by Nguyen Trieu 2022</h5>
+    </div>
   )
 }
 
